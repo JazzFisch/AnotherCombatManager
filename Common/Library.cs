73dc50c1405cf3586bb2908287be595a616b6625
Jason Fischer <jazzfisch@outlook.com>
2013-11-04 09:31:59 -0800

    Begin adding Library store.

---
+++ Common/Library.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnD4e.LibraryHelper.Common {
+    public class Library {
+        public static void DeleteLibrary (string name) {
+            var path = Assembly.GetExecutingAssembly().Location;
+            path = Path.Combine(Path.GetDirectoryName(path), name);
+            File.Delete(path);
+        }
+
+        public static Library OpenLibrary (string name) {
+            var path = Assembly.GetExecutingAssembly().Location;
+            return Library.OpenLibrary(Path.GetDirectoryName(path), name);
+        }
+
+        public static Library OpenLibrary (string path, string name) {
+            Library library = new Library();
+            return library;
+        }
+
+        public void Put (Combatant combatant) {
+        }
+
+        public void Remove (Combatant combatant) {
+        }
+
+        public void Set (Combatant combatant) {
+        }
+    }
+}
