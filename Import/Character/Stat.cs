4c5f0ca273458405473199ebfe7d832b410b4111
Jason Fischer <jazzfisch@outlook.com>
2013-11-11 10:43:02 -0800

    Missing file from last commit.

--- Import/Character/Stat.cs
+++ Import/Character/Stat.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Xml.Serialization;
+using DnD4e.LibraryHelper.Import.Common;
 
 namespace DnD4e.LibraryHelper.Import.Character {
     public class Stat {
