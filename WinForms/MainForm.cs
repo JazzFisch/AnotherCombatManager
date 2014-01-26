168ac6b51dd0b4bb5330eaec7352a58000285e75
Jason Fischer <jazzfisch@outlook.com>
2014-01-25 22:23:26 -0800

    Remove unused project references.

--- WinForms/MainForm.cs
+++ WinForms/MainForm.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
-namespace AnotherCM.WinForms {
-    public partial class MainForm : Form {
-        public MainForm () {
+namespace AnotherCM.WinForms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
             InitializeComponent();
         }
     }
